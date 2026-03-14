cbb8839484c70de2b5b45dadb718c2383c4a1052
Komal Pathare <117629456+komalpathare8755@users.noreply.github.com>
2026-03-14 15:49:17 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,22 @@
-public class Main{
-    public static void main(String[] args){
-        int n=5;
+public class Main {
+    public static void main(String[] args) {
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j);
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
             }
-          
-            for(int j=i-1;j>=1;j--){
-                System.out.print(j);
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
-             
+
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
             System.out.println();
         }
     }
